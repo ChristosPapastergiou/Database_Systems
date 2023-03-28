f5205efddd0db2ee0afba53594b2e2bf2dde9032
ChristosPapastergiou <xrhstoguitar@gmail.com>
2023-03-28 20:45:27 +0300

    commit

--- include/sht_table.h
+++ include/sht_table.h
@@ -36,33 +36,27 @@ int SHT_CreateSecondaryIndex(char *sfileName, int buckets, char* fileName);
 /* Η συνάρτηση SHT_OpenSecondaryIndex ανοίγει το αρχείο με όνομα sfileName
 και διαβάζει από το πρώτο μπλοκ την πληροφορία που αφορά το δευτερεύον
 ευρετήριο κατακερματισμού.*/
+
+// Opens the file named sfilename and reads from the first  
+// block the information about the secondary hashtable file
 SHT_info* SHT_OpenSecondaryIndex(char *sfileName);
 
-/*Η συνάρτηση SHT_CloseSecondaryIndex κλείνει το αρχείο που προσδιορίζεται
-μέσα στη δομή header_info. Σε περίπτωση που εκτελεστεί επιτυχώς, επιστρέφεται
-0, ενώ σε διαφορετική περίπτωση -1. Η συνάρτηση είναι υπεύθυνη και για την
-αποδέσμευση της μνήμης που καταλαμβάνει η δομή που περάστηκε ως παράμετρος,
-στην περίπτωση που το κλείσιμο πραγματοποιήθηκε επιτυχώς.*/
+// Closes the file specified in in the header_info structure
+// The function is is also responsible for freeing the memory occupied
+// by the structure passed as a parameter, in case the closure was successfully performed
+// Return 0 if successfull, -1 if failure
 int SHT_CloseSecondaryIndex(SHT_info* header_info);
 
-/*Η συνάρτηση SHT_SecondaryInsertEntry χρησιμοποιείται για την εισαγωγή μιας
-εγγραφής στο αρχείο κατακερματισμού. Οι πληροφορίες που αφορούν το αρχείο
-βρίσκονται στη δομή header_info, ενώ η εγγραφή προς εισαγωγή προσδιορίζεται
-από τη δομή record και το block του πρωτεύοντος ευρετηρίου που υπάρχει η εγγραφή
-προς εισαγωγή. Σε περίπτωση που εκτελεστεί επιτυχώς, επιστρέφεται 0, ενώ σε
-διαφορετική περίπτωση -1.*/
+// Insert a entry into the hashtable file, the information about the file is in the
+// header_info structure while the record to be inserted is specified by the record structure
+// Return 0 if successfull, -1 if failure
 int SHT_SecondaryInsertEntry(SHT_info* header_info, Record record, int block_id);
 
-/*Η συνάρτηση αυτή χρησιμοποιείται για την εκτύπωση όλων των εγγραφών που
-υπάρχουν στο αρχείο κατακερματισμού οι οποίες έχουν τιμή στο πεδίο-κλειδί
-του δευτερεύοντος ευρετηρίου ίση με name. Η πρώτη δομή περιέχει πληροφορίες
-για το αρχείο κατακερματισμού, όπως αυτές είχαν επιστραφεί κατά το άνοιγμά
-του. Η δεύτερη δομή περιέχει πληροφορίες για το δευτερεύον ευρετήριο όπως
-αυτές είχαν επιστραφεί από την SHT_OpenIndex. Για κάθε εγγραφή που υπάρχει
-στο αρχείο και έχει όνομα ίσο με value, εκτυπώνονται τα περιεχόμενά της
-(συμπεριλαμβανομένου και του πεδίου-κλειδιού). Να επιστρέφεται επίσης το
-πλήθος των blocks που διαβάστηκαν μέχρι να βρεθούν όλες οι εγγραφές. Σε
-περίπτωση λάθους επιστρέφει -1.*/
+// Print all records that exist in the hashtable file that have a value in key field equal to name
+// The first structure gives information about the hashtable and the second gives information about the secondary hashtable
+// For each record that exists in the file and has a value in the id field equal to value, print it
+// Also return the number of blocks that read until all records are found
+// Return the number of readed blocks if successfull, -1 if failure
 int SHT_SecondaryGetAllEntries(HT_info* ht_info, SHT_info* header_info, char* name);
 
 #endif
