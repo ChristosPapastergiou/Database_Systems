c783fdd5b30fd728ee20595496d1c681de2ec1db
ChristosPapastergiou <xrhstoguitar@gmail.com>
2023-03-28 19:34:38 +0300

    commit

--- include/bf.h
+++ include/bf.h
@@ -47,82 +47,48 @@ char* BF_Block_GetData(const BF_Block *block);
 // Initialize the BF layer. We can choose between two Block replacement policies (LRU, MRU)
 BF_ErrorCode BF_Init(const ReplacementAlgorithm repl_alg);
 
-/*
- * Η συνάρτηση BF_CreateFile δημιουργεί ένα αρχείο με όνομα filename το
- * οποίο αποτελείται από blocks. Αν το αρχείο υπάρχει ήδη τότε επιστρέφεται
- * κωδικός λάθους. Σε περίπτωση επιτυχούς εκτέλεσης της συνάρτησης επιστρέφεται
- * BF_OK, ενώ σε περίπτωση αποτυχίας επιστρέφεται κωδικός λάθους. Αν θέλετε να
- * δείτε το είδος του λάθους μπορείτε να καλέσετε τη συνάρτηση BF_PrintError.
- */
+// Creates a file named filename which consists of blocks. If the file already exists then an error code is returned
+// Returns BF_OK if successfull or an error code if failed 
+// Call the BF_PrintError function to see the error
 BF_ErrorCode BF_CreateFile(const char* filename);
 
-/*
- * Η συνάρτηση BF_OpenFile ανοίγει ένα υπάρχον αρχείο από blocks με όνομα
- * filename και επιστρέφει το αναγνωριστικό του αρχείου στην μεταβλητή
- * file_desc. Σε περίπτωση επιτυχίας επιστρέφεται BF_OK ενώ σε περίπτωση
- * αποτυχίας, επιστρέφεται ένας κωδικός λάθους. Αν θέλετε να δείτε το είδος
- * του λάθους μπορείτε να καλέσετε τη συνάρτηση BF_PrintError.
- */
+// Opens an existing file from blocks named filename and returns the file ID in the file_desc variable
+// Returns BF_OK if successfull or an error code if failed
+// Call the BF_PrintError function to see the error
 BF_ErrorCode BF_OpenFile(const char* filename, int *file_desc);
 
-/*
- * Η συνάρτηση BF_CloseFile κλείνει το ανοιχτό αρχείο με αναγνωριστικό αριθμό
- * file_desc. Σε περίπτωση επιτυχίας επιστρέφεται BF_OK ενώ σε περίπτωση
- * αποτυχίας, επιστρέφεται ένας κωδικός λάθους. Αν θέλετε να δείτε το
- * είδος του λάθους μπορείτε να καλέσετε τη συνάρτηση BF_PrintError.
- */
+// Close the open file with file ID file_desc
+// Returns BF_OK if successfull or an error code if failed
+// Call the BF_PrintError function to see the error
 BF_ErrorCode BF_CloseFile(const int file_desc);
 
-/*
- * Η συνάρτηση Get_BlockCounter δέχεται ως όρισμα τον αναγνωριστικό αριθμό
- * file_desc ενός ανοιχτού αρχείου από block και βρίσκει τον αριθμό των
- * διαθέσιμων blocks του, τον οποίο και επιστρέφει στην μεταβλητή blocks_num.
- * Σε περίπτωση επιτυχίας επιστρέφεται BF_OK ενώ σε περίπτωση αποτυχίας,
- * επιστρέφεται ένας κωδικός λάθους. Αν θέλετε να δείτε το είδος του λάθους
- * μπορείτε να καλέσετε τη συνάρτηση BF_PrintError.
- */
+// Taking file ID of an open file and finds the number of available block, and return it to block_num variable
+// Returns BF_OK if successfull or an error code if failed
+// Call the BF_PrintError function to see the error
 BF_ErrorCode BF_GetBlockCounter(const int file_desc, int *blocks_num);
 
-/*
- * Με τη συνάρτηση BF_AllocateBlock δεσμεύεται ένα καινούριο block για το
- * αρχείο με αναγνωριστικό αριθμό blockFile. Το νέο block δεσμεύεται πάντα
- * στο τέλος του αρχείου, οπότε ο αριθμός του block είναι
- * BF_getBlockCounter(file_desc) - 1. Το block που δεσμεύεται καρφιτσώνεται
- * στην μνήμη (pin) και επιστρέφεται στην μεταβλητή block. Όταν δεν το
- * χρειαζόμαστε άλλο αυτό το block τότε πρέπει να ενημερώσουμε τον επίπεδο
- * block καλώντας την συνάρτηση BF_UnpinBlock. Σε περίπτωση επιτυχίας
- * επιστρέφεται BF_OK ενώ σε περίπτωση αποτυχίας, επιστρέφεται ένας κωδικός
- * λάθους. Αν θέλετε να δείτε το είδος του λάθους μπορείτε να καλέσετε τη
- * συνάρτηση BF_PrintError.
- */
+// Allocate a new block for the file with ID number blockFile. The new block is always bound at the end of the file,
+// so the block number is BF_getBlockCounter(file_desc) - 1. The bound block is pinned to memory 
+// (pin) and returned to the block variable. When we no longer need this block 
+// then we need to update the block level by calling the function BF_UnpinBlock
+// Returns BF_OK if successfull or an error code if failed
+// Call the BF_PrintError function to see the error 
 BF_ErrorCode BF_AllocateBlock(const int file_desc, BF_Block *block);
 
-
-/*
- * Η συνάρτηση BF_GetBlock βρίσκει το block με αριθμό block_num του ανοιχτού
- * αρχείου file_desc και το επιστρέφει στην μεταβλητή block. Το block που
- * δεσμεύεται καρφιτσώνεται στην μνήμη (pin). Όταν δεν χρειαζόμαστε άλλο αυτό
- * το block τότε πρέπει να ενημερώσουμε τον επίπεδο block καλώντας την συνάρτηση
- * BF_UnpinBlock. Σε περίπτωση επιτυχίας επιστρέφεται BF_OK ενώ σε περίπτωση
- * αποτυχίας, επιστρέφεται ένας κωδικός λάθους. Αν θέλετε να δείτε το είδος του
- * λάθους μπορείτε να καλέσετε τη συνάρτηση BF_PrintError.
- */
+// Finds the block with block_num number of the open file_desc and returns it in the block variable
+// The block bound is pinned to memory (pin). When we no longer need this block 
+// then we need to update the block level by calling the function BF_UnpinBlock
+// Returns BF_OK if successfull or an error code if failed
+// Call the BF_PrintError function to see the error 
 BF_ErrorCode BF_GetBlock(const int file_desc, const int block_num, BF_Block *block);
 
-/*
- * Η συνάρτηση BF_UnpinBlock αποδεσμεύει το block από το επίπεδο Block το
- * οποίο κάποια στηγμή θα το γράψει στο δίσκο. Σε περίπτωση επιτυχίας
- * επιστρέφεται BF_OK ενώ σε περίπτωση αποτυχίας, επιστρέφεται ένας κωδικός
- * λάθους. Αν θέλετε να δείτε το είδος του λάθους μπορείτε να καλέσετε τη
- * συνάρτηση BF_PrintError.
- */
+// Releases the block from the Block layer which some arrow will write to disk
+// Returns BF_OK if successfull or an error code if failed
+// Call the BF_PrintError function to see the error 
 BF_ErrorCode BF_UnpinBlock(BF_Block *block);
 
-/*
- * Η συνάρτηση BF_PrintError βοηθά στην εκτύπωση των σφαλμάτων που δύναται να
- * υπάρξουν με την κλήση συναρτήσεων του επιπέδου αρχείου block. Εκτυπώνεται
- * στο stderr μια περιγραφή του πιο σφάλματος.
- */
+// Helps to print the errors that may occur when calling block level functions
+// A description of the most error is printed to stderr 
 void BF_PrintError(BF_ErrorCode err);
 
 // Calls the Block layer by writing to disk any block had in memory
